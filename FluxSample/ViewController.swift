d6751d4cdf64f07f6905f831e46adc2e0e0be614
fummicc1 <fumiya.tennis1@gmail.com>
2019-09-08 09:33:31 +0900

    Initial Commit

---
+++ FluxSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FluxSample
+//
+//  Created by Fumiya Tanaka on 2019/09/08.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
