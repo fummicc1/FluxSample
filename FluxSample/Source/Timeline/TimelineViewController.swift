4a3e6eae224d6f5e27dd93b38e6a6582b5deb6a9
fummicc1 <fumiya.tennis1@gmail.com>
2019-09-10 18:40:17 +0900

    画面遷移Done - TimelineからAddMemo画面へ画面遷移をできるようになった。 - Timeline画面を作成

--- FluxSample/Source/Timeline/TimelineViewController.swift
+++ FluxSample/Source/Timeline/TimelineViewController.swift
@@ -21,15 +21,40 @@ class TimelineViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupViewLayout()
+    }
+    
+    private func setupViewLayout() {
+        addMemoButton.layer.cornerRadius = addMemoButton.frame.height / 2
+        addMemoButton.layer.masksToBounds = true
     }
     
     private func configure() {
-        let scopeDriver = scopeSegmentedControl.rx.value.asDriver()
-        let addMemoButtonDriver = addMemoButton.rx.tap.asDriver()
+        let scopeObservable = scopeSegmentedControl.rx.value.asObservable()
+        let addMemoButtonObservable = addMemoButton.rx.tap.asObservable()
         
         dataSource = TimelineDataSource()
         
-        dataSource?.store.moveToAdd.bind(to: moveToAddScene).disposed(by: disposeBag)
+        dataSource?.store
+            .moveToAdd
+            .bind(to: moveToAddScene)
+            .disposed(by: disposeBag)
+        
+        scopeObservable.subscribe { [weak self] (event) in
+            guard let self = self, let value = event.element else { return }
+            let scope: DisclosureScope = value == 0 ? .publicScope : .privateScope
+            self.dataSource?.actionCreator.changeScope(scope: scope)
+        }.disposed(by: disposeBag)
+        
+        addMemoButtonObservable.subscribe { [weak self] (event) in
+            guard let self = self else { return }
+            self.dataSource?.actionCreator.moveToAddScene()
+        }.disposed(by: disposeBag)
     }
 }
 
